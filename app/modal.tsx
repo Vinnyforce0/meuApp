44f0eb44b48bda6713df20631f545a3fe44db818
Vinnyforce0 <jorgevinny2008@gmail.com>
2025-12-03 14:41:37 -0300

    final

--- app/modal.tsx
+++ app/modal.tsx
@@ -9,7 +9,7 @@ export default function ModalScreen() {
     <ThemedView style={styles.container}>
       <ThemedText type="title">This is a modal</ThemedText>
       <Link href="/" dismissTo style={styles.link}>
-        <ThemedText type="link">Go to home screeeeeeeeeeeeeeeeeen</ThemedText>
+        <ThemedText type="link">Go to home buregodego</ThemedText>
       </Link>
     </ThemedView>
   );
